89ed602f8694d46f73a277be3963daedf632d456
Tole15 <rodrigermoon@gmail.com>
2023-08-02 13:40:41 -0600

    Add userType, userName, and userEmail props to ProfileClientSimpleCard
    This commit introduces three new optional string props to the ProfileClientSimpleCard component: userType, userName, and userEmail. These props allow for more flexible usage of the component, enabling customization of the displayed user information.
    
    The types for these props have been defined in the ProfileClientSimpleCardTypes file within the schema folder inside the src directory, providing a clear and organized structure for component prop definitions.

--- CtasApp/src/components/ProfileClientSimpleCard.tsx
+++ CtasApp/src/components/ProfileClientSimpleCard.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
+import { ProfileClientSimpleCardProps } from '../schema/ProfileClientSimpleCardSchema'; 
 import ProfileClientSimpleCardStyles from '../styles/ProfileClientSimpleCardStyles';
 
-const ProfileClientSimpleCard = () => {
+const ProfileClientSimpleCard: React.FC<ProfileClientSimpleCardProps> = ({
+  userType = 'Client',
+  userName = 'Jessica Jhonson', 
+  userEmail = 'jessica@correo.com', 
+}) => {
   return (
     <View style={ProfileClientSimpleCardStyles.cardContainer}>
-        <Text style={ProfileClientSimpleCardStyles.userType}>Client</Text>
-        <View style={ProfileClientSimpleCardStyles.userImageContainer}>
-      <Image
-        source={{
-          uri: 'https://images.pexels.com/photos/15212752/pexels-photo-15212752/free-photo-of-chica-bangkok.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        }}
-        style={ProfileClientSimpleCardStyles.userImage}
-      />
+      <Text style={ProfileClientSimpleCardStyles.userType}>{userType}</Text>
+      <View style={ProfileClientSimpleCardStyles.userImageContainer}>
+        <Image
+          source={{
+            uri: 'https://images.pexels.com/photos/15212752/pexels-photo-15212752/free-photo-of-chica-bangkok.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+          }}
+          style={ProfileClientSimpleCardStyles.userImage}
+        />
       </View>
-      <Text style={ProfileClientSimpleCardStyles.userName}>Jessica Jhonson</Text>
-        <Text style={ProfileClientSimpleCardStyles.userEmail}>Email: javier@correo.com</Text>
+      <Text style={ProfileClientSimpleCardStyles.userName}>{userName}</Text>
+      <Text style={ProfileClientSimpleCardStyles.usegit rEmail}>Email: {userEmail}</Text>
     </View>
   );
 };
