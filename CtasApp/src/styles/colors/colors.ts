dee8e0482a4bb9aa8531fa7d1ed5b9c371b9dcd9
Tole15 <rodrigermoon@gmail.com>
2023-08-02 07:28:52 -0600

    feat(styles/colors): define primary color palette for the application
    This commit introduces a centralized `colors` file within the `styles/colors` directory. It contains the definitions for the main colors to be used across the application, including white, aqua, babyBlue, skyBlue, mint, pistachio, and black.
    By centralizing these color definitions, we ensure consistency and ease of maintenance for the color scheme throughout the application.
    Path: `src/styles/colors/colors.ts`
    
    Co-authored-by: DerekAyala <derek.ayala12345@hotmail.com>
    Co-authored-by: Eduardoespl <eduardoesp3317@gmail.com>
    Co-authored-by: Antonio3veces3 <aantonioramirez33@gmail.com>

---
+++ CtasApp/src/styles/colors/colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+    white: '#FFFFFF',
+    aqua: '#40B9A9',
+    babyBlue: '#99C3DC',
+    skyBlue: '#73C4CF',
+    mint: '#86D5B7',
+    pistachio: '#64C989',
+    black: '#000000',
+};
+
+export default colors;
