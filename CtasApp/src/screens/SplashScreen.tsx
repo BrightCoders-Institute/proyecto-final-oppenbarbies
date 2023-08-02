053de342be06a350314686fcc7e160f68f9fd308
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-02 16:46:57 -0600

    add SplashScreen image and logo test

--- CtasApp/src/screens/SplashScreen.tsx
+++ CtasApp/src/screens/SplashScreen.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 
 type Props = {};
 
 const SplashScreen: React.FC<Props> = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>SplashScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
