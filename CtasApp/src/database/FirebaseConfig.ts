d9bcc981670338a89aa5520d679403b77444a403
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-03 19:18:52 -0600

    Configured Firebase and Installed React Native Config for Securing Firebase Keys
    - Set up Firebase in the React Native project with the necessary configurations to enable authentication, real-time database, or Firestore, depending on project requirements.
    - Installed the "react-native-config" package to manage and hide sensitive Firebase keys and configurations from version control.
    - Created a .env file to store the Firebase API keys, project ID, and other sensitive information.
    - Updated the project settings and configurations to use the environment variables from the .env file securely.
    - Added a .env.example file as a template to demonstrate the structure of the .env file and show an example of how to define the Firebase keys and configurations.
    
    Co-authored-by: Antonio3veces3 <aantonioramirez33@gmail.com>
    Co-authored-by: Tole15 <rodrigermoon@gmail.com>

--- CtasApp/src/database/FirebaseConfig.ts
+++ CtasApp/src/database/FirebaseConfig.ts
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import Config from "react-native-config";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBV4rLfCwHNO_mMBvg1vVqwR-E-Cr-5nMU",
-  authDomain: "ctas-f9a95.firebaseapp.com",
-  projectId: "ctas-f9a95",
-  storageBucket: "ctas-f9a95.appspot.com",
-  messagingSenderId: "65508546835",
-  appId: "1:65508546835:web:52c4418966761893e462e6"
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  appId: Config.APP_ID,
 };
 
 // Initialize Firebase
