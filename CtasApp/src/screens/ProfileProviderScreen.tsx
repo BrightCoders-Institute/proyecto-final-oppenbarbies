4eaa1168b9dd105c975d08eb8c71d8ccb62b6145
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-08 13:22:12 -0600

    Returned files to his original form

--- CtasApp/src/screens/ProfileProviderScreen.tsx
+++ CtasApp/src/screens/ProfileProviderScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import ProviderInformation from '../components/ProviderInformation';
 
 type Props = {};
 
@@ -8,7 +7,6 @@ const ProfileProviderScreen: React.FC<Props> = () => {
   return (
     <View>
       <Text>ProfileProviderScreen</Text>
-      <ProviderInformation name='Lic. Valeriano Perez' age={26} location='Colima, Villa de Álvarez '/>
     </View>
   );
 };
