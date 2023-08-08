1453371e3b15212f8704f26230f70b756c4efc5d
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-08 16:43:09 -0600

    Converted Provider Info Card to Reusable Component with Prop Schema
    - Transformed the service provider information card into a reusable component named "ProviderInfoCard."
    - Created a separate file named "ProviderInfoCardSchema" to define the prop types that the component receives.

--- CtasApp/src/screens/SearchScreen.tsx
+++ CtasApp/src/screens/SearchScreen.tsx
@@ -1,16 +1,8 @@
-import React, {useEffect} from 'react';
-import {
-  SafeAreaView,
-  Text,
-  View,
-  TextInput,
-  Pressable,
-  Image,
-} from 'react-native';
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
 import {SearchStyles} from '../styles/SearchStyles';
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import SearchInput from '../components/SearchInput';
-import {SearchCardStyles} from '../styles/SearchCardStyles';
+import ProviderInfoCard from '../components/ProviderInfoCard';
 type Props = {};
 
 const SearchScreen: React.FC<Props> = () => {
@@ -21,36 +13,15 @@ const SearchScreen: React.FC<Props> = () => {
       </View>
       <View style={SearchStyles.bodyContainer}>
         <SearchInput />
-        <View style={SearchCardStyles.cardContainer}>
-          <View>
-            <Image
-              source={require('../img/lic.jpg')}
-              style={SearchCardStyles.imgProfile}
-            />
-          </View>
-          <View style={SearchCardStyles.infoContainer}>
-            <View style={SearchCardStyles.infoHeader}>
-              <Text style={SearchCardStyles.name}>Lic. Valeriano Perez</Text>
-              <View style={[SearchCardStyles.row, SearchCardStyles.gap]}>
-                <View style={SearchCardStyles.row}>
-                  <Ionicons name='location-sharp' size={25} style={SearchCardStyles.icon}/>
-                  <Text style={SearchCardStyles.infoText}>Colima</Text>
-                </View>
-                <View style={SearchCardStyles.row}>
-                  <Ionicons name='person-circle' size={25} style={SearchCardStyles.icon}/>
-                  <Text style={SearchCardStyles.infoText}>Age: 26</Text>
-                </View>
-              </View>
-              <Text style={SearchCardStyles.infoDescription}>Personalized counseling in family cases</Text>
-            </View>
-            <View style={SearchCardStyles.row}>
-              <Text style={SearchCardStyles.infoText}>Lawyer</Text>
-            </View>
-          </View>
-        </View>
+        <ProviderInfoCard
+          name="Lic. Valeriano Perez"
+          age={26}
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, velit fuga hic modi quam."
+          location="Colima"
+          ocupation="Lawyer"
+        />
       </View>
     </SafeAreaView>
   );
 };
-
 export default SearchScreen;
