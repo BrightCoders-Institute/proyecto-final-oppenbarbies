2e2bd0e7e87bc25c3f5452589289910819a61bdb
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-08 13:22:38 -0600

    Refactored Search Input as a Reusable Component
    - Transformed the search input functionality into a standalone and reusable component.
    - Created a custom "SearchInput" component that includes an input field and a search icon button.

--- CtasApp/src/screens/SearchScreen.tsx
+++ CtasApp/src/screens/SearchScreen.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import {SafeAreaView, Text, View, TextInput, Pressable} from 'react-native';
 import {SearchStyles} from '../styles/SearchStyles';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import SearchInput from '../components/SearchInput';
 type Props = {};
 
 const SearchScreen: React.FC<Props> = () => {
@@ -11,14 +12,7 @@ const SearchScreen: React.FC<Props> = () => {
         <Text style={SearchStyles.title}>What do you need?</Text>
       </View>
       <View style={SearchStyles.bodyContainer}>
-        <View style={SearchStyles.inputContainer}>
-          <TextInput
-            style={SearchStyles.textInput}
-            placeholder="Doctor, Lawyer..."></TextInput>
-            <Pressable>
-              <FontAwesome name="search" size={25} style={SearchStyles.icon}/>
-            </Pressable>
-        </View>
+        <SearchInput/>
       </View>
     </SafeAreaView>
   );
