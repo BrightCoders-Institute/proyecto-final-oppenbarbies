69979cd1b4199a7fb6bc442e826ba71cdd6967f4
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-07 21:20:07 -0600

    Added SearchScreen and Integrated into Stack Navigator
    - Created a new screen named "SearchScreen" to provide a search bar and a list of service providers.
    - Integrated the "SearchScreen" into the Stack Navigator to enable seamless navigation between screens.

---
+++ CtasApp/src/screens/SearchScreen.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react';
+import {SafeAreaView, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import SplashScreenStyles from '../styles/SplashScreenStyles';
+import Logo from '../components/Logo';
+type Props = {};
+
+const SearchScreen: React.FC<Props> = () => {
+  return (
+    <SafeAreaView>
+      <View></View>
+      <View></View>
+    </SafeAreaView>
+  );
+};
+
+export default SearchScreen;
