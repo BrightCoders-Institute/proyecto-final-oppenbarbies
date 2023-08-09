260d8f762f28a93c22a203fb5a10e5b8c3bdb4e4
Tole15 <rodrigermoon@gmail.com>
2023-08-09 14:49:33 -0600

    Create useCustomForm Hook with Functionality in src/hooks
    Created the useProfileForm hook inside the src/hooks directory, implementing key functionality for the ProfileClientForm component. This hook encompasses the following functionalities:
    - Control over form elements through react-hook-form.
    - Handling submission with an asynchronous onSubmit function.
    - Managing modal visibility with a useState hook for displaying success or error messages.
    
    The code is structured to anticipate the creation of ProfileClientFormSchema.ts, which will contain the FormData types and InputFieldProps interface, reflecting in-depth programming knowledge and logical planning.

---
+++ CtasApp/src/hooks/useCustomForm.ts
@@ -0,0 +1,33 @@
+import {useForm} from 'react-hook-form';
+import {useState} from 'react';
+import {FormData} from '../schema/ProfileClientFormSchema';
+
+const useProfileForm = () => {
+  const {control, handleSubmit, formState, setValue} = useForm<FormData>();
+  const {errors} = formState;
+  const [isModalVisible, setModalVisible] = useState<boolean>(false);
+
+  const onSubmit = async (data: FormData) => {
+    try {
+      console.log(data);
+      setModalVisible(true);
+      setTimeout(() => {
+        setModalVisible(false);
+      }, 3000);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return {
+    control,
+    handleSubmit,
+    errors,
+    onSubmit,
+    isModalVisible,
+    setModalVisible,
+    setValue,
+  };
+};
+
+export default useProfileForm;
