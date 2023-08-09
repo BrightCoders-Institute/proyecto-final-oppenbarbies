a85bff788fdbc1e60914635bf4f23b89526ba55e
Tole15 <rodrigermoon@gmail.com>
2023-08-09 14:26:04 -0600

    Refactoring of ProfileClientForm
    Updated the ProfileClientForm.tsx component to incorporate several enhancements:
    - Integration of 'react-hook-form'.
    - Implementation of a custom hook, useCustomForm, for validations and error display in <InputField>s.
    - Addition of a <LocationInput> component that, through calls to a geolocation API, completes addresses.
    - Introduction of a <MessageModal> component as a loading animation that is customizable and reusable.
    - Inclusion of <CalendarModal> to handle user's date of birth selection.
    
    Please note that:
    - useCustomForm, LocationInput, and MessageModal are still under development but planned in the current code based on future development vision and prior knowledge of the libraries to be used.
    - Although 'react-hook-form' is not yet installed, it has been coded with prior knowledge of using the Controller from this library.
    - The styles according to the recent additions will be updated in future commits.

--- CtasApp/src/components/ProfileClientForm.tsx
+++ CtasApp/src/components/ProfileClientForm.tsx
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable, Modal} from 'react-native';
+import {Controller} from 'react-hook-form';
+import useCustomForm from '../hooks/useCustomForm';
 import InputField from './InputField';
 import ProfileClientFormStyles from '../styles/ProfileClientFormStyles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Button from './Button';
+import LocationInput from './LocationInput';
+import MessageModal from './MessageModal';
+import CalendarModal from './CalendarModal';
 
 const ProfileClientForm: React.FC = () => {
-  const [name, setName] = useState<string>('');
-  const [phone, setPhone] = useState<string>('');
-  const [birthDate, setBirthDate] = useState<string>('');
-  const [location, setLocation] = useState<string>('');
+  const {control, handleSubmit, errors, onSubmit, setValue, isModalVisible} =
+    useCustomForm();
+  const [birthdate, setBirthdate] = useState('');
+  const [isDateModalVisible, setDateModalVisible] = useState<boolean>(false);
+  const [isLocationModalVisible, setLocationModalVisible] =
+    useState<boolean>(false);
+
+  const handleDateModalOpen = () => {
+    setDateModalVisible(true);
+  };
+  const handleDateModalClose = () => {
+    setDateModalVisible(false);
+  };
+
+  const handleSetBirthdate = (date: string) => {
+    setValue('birthDate', date);
+  };
 
   return (
     <View style={ProfileClientFormStyles.container}>
@@ -22,47 +40,144 @@ const ProfileClientForm: React.FC = () => {
           This name is visible to all app users.
         </Text>
       </View>
-      <InputField
-        label="Name"
-        value={name}
-        onChangeText={setName}
-        placeholder="Enter your name"
+      <Controller
+        control={control}
+        render={({field: {onChange, value}}) => (
+          <InputField
+            label="Name"
+            value={value}
+            onChangeText={value => {
+              if (errors.name) {
+                setValue('name', '');
+              }
+              onChange(value);
+            }}
+            placeholder="Enter your name"
+            errorMessage={errors.name?.message}
+          />
+        )}
+        name="name"
+        rules={{
+          required: 'Name is required!',
+          pattern: {
+            value: /^[A-Za-z\s]+$/,
+            message: 'Name can only contain letters and spaces!',
+          },
+        }}
+        defaultValue=""
       />
       <Text style={ProfileClientFormStyles.text}> Phone: </Text>
-      <InputField
-        label="Phone"
-        value={phone}
-        onChangeText={setPhone}
-        placeholder="Enter your phone number"
+      <Controller
+        control={control}
+        render={({field: {onChange, value}}) => (
+          <InputField
+            label="Phone"
+            value={value}
+            onChangeText={value => {
+              if (errors.phone) {
+                setValue('phone', '');
+              }
+              onChange(value);
+            }}
+            placeholder="Enter your phone number"
+            errorMessage={errors.phone?.message}
+          />
+        )}
+        name="phone"
+        rules={{
+          required: 'Phone number is required!',
+          pattern: {
+            value: /^\d{10}$/,
+            message: 'Phone number must be 10 digits!',
+          },
+        }}
+        defaultValue=""
       />
       <Text style={ProfileClientFormStyles.text}> Birth Date: </Text>
-      <InputField
-        label="BirthDate"
-        value={birthDate}
-        onChangeText={setBirthDate}
-        placeholder="Enter your Birth Date"
-      />
-      <AntDesign
-        name="calendar"
-        size={20}
-        color="#40B9A9"
-        style={ProfileClientFormStyles.calendarIcon}
-      />
-      <Text style={ProfileClientFormStyles.text}> Location: </Text>
-      <InputField
-        label="Location"
-        value={location}
-        onChangeText={setLocation}
-        placeholder="Enter your location"
-      />
-      <View style={ProfileClientFormStyles.buttonContainer}>
-        <Button
-          text="Save"
-          onPress={function (): void {
-            throw new Error('Function not implemented.');
-          }}
+      <Pressable
+        onPress={handleDateModalOpen}
+        style={ProfileClientFormStyles.textInputPress}>
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputField
+              label="BirthDate"
+              value={value || birthdate}
+              onChangeText={value => onChange(value)}
+              placeholder="Enter your Birth Date"
+              errorMessage={errors.birthDate?.message}
+              editable={false}
+            />
+          )}
+          name="birthDate"
+          rules={{required: 'Birth Date is required!'}}
+          defaultValue=""
         />
+      </Pressable>
+      <Pressable
+        style={ProfileClientFormStyles.iconPress}
+        onPress={handleDateModalOpen}>
+        <AntDesign name="calendar" size={20} color="#40B9A9" />
+      </Pressable>
+      <Text style={ProfileClientFormStyles.text}> Location: </Text>
+      <Pressable
+        onPress={() => setLocationModalVisible(true)}
+        style={ProfileClientFormStyles.textInputPress}>
+        <Controller
+          control={control}
+          render={({field: {onChange, value}}) => (
+            <InputField
+              label="Location"
+              value={value}
+              onChangeText={item => onChange(item)}
+              placeholder="Enter your location"
+              editable={false}
+            />
+          )}
+          name="location"
+          rules={{required: 'Location is required!'}}
+          defaultValue=""
+        />
+      </Pressable>
+      {isLocationModalVisible ? (
+        <Modal animationType="slide" transparent={true}>
+          <View style={ProfileClientFormStyles.modalContainer}>
+            <LocationInput
+              onSelected={item => {
+                setLocationModalVisible(false);
+                setValue('location', item);
+              }}
+              errorMessage={errors.location?.message} // Passing the error message here
+            />
+            <Button
+              text="Close"
+              onPress={() => setLocationModalVisible(false)}
+              styleName="default"
+            />
+          </View>
+        </Modal>
+      ) : null}
+      <View style={ProfileClientFormStyles.buttonContainer}>
+        <Button text="Save" onPress={handleSubmit(onSubmit)} />
       </View>
+      <MessageModal
+        isVisible={isModalVisible}
+        animationKey="loading"
+        title="Saving..."
+        message="Please wait while we save your profile data."
+      />
+      {isDateModalVisible ? (
+        <Modal animationType="slide" transparent={true}>
+          <View style={ProfileClientFormStyles.modalContainer}>
+            <CalendarModal setBirthdate={handleSetBirthdate} />
+            <Button
+              text="Close"
+              onPress={handleDateModalClose}
+              styleName="default"
+            />
+          </View>
+        </Modal>
+      ) : null}
     </View>
   );
 };
