b28abccb3f6d4300332207172d55350fa6ce6d25
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-10 12:38:32 -0600

    Rename from HomeProviderScreen.tsx to SetCitaScreen.tsx
    Renamed screen name from HomeProviderScreen.tsx to SetCitaScreen.tsx, because HomeProviderScreen is not going to be used anymore.

---
+++ CtasApp/src/screens/SetCitaScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const SetCitaScreen: React.FC = () => {
+  return (
+    <View>
+      <Text>SetCitaScreen</Text>
+    </View>
+  );
+};
+
+export default SetCitaScreen;
