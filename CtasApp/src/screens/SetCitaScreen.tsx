dae65843730ab5e311bf9b6c6fd96879284ca11d
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-10 13:06:57 -0600

    Added ProviderInformation component
    Updated SetCitaScreen to use ProviderInformation component
    Added the space that will be uses for the map and calendar components
    Created the back arrow button, for style only

--- CtasApp/src/screens/SetCitaScreen.tsx
+++ CtasApp/src/screens/SetCitaScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import ProviderInformation from '../components/ProviderInformation';
+import ProviderSetCitaStyles from '../styles/ProviderSetCitaStyles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const SetCitaScreen: React.FC = () => {
   return (
-    <View>
-      <Text>SetCitaScreen</Text>
+    <View style={ProviderSetCitaStyles.main}>
+      <View style={ProviderSetCitaStyles.backArrow}>
+        <Ionicons name='arrow-back' size={20} color='#ffff' />
+      </View>
+      <ProviderInformation name='Lic. Valeriano Perez' age={26} location='Villa de Alvarez, Col'/>
+      <View style={ProviderSetCitaStyles.body}>
+        <Text style={ProviderSetCitaStyles.appointmentDetails}>Office Location</Text>
+      </View>
     </View>
   );
 };
--- CtasApp/src/styles/ProviderSetCitaStyles.ts
+++ CtasApp/src/styles/ProviderSetCitaStyles.ts
@@ -2,6 +2,19 @@ import {StyleSheet} from 'react-native';
 import Colors from './colors/Colors';
 
 const ProviderSetCitaStyles = StyleSheet.create({
+  main:{
+    flex: 1,
+    backgroundColor: Colors.aqua,
+  },
+  body: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    marginTop: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    alignItems: 'center',
+    padding: 10,
+  },
   providerCard: {
     flexDirection: 'row',
     justifyContent: 'space-around',
@@ -40,6 +53,20 @@ const ProviderSetCitaStyles = StyleSheet.create({
     width: '40%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  appointmentDetails: {
+    fontFamily: 'Inter-ExtraBold',
+    fontSize: 15,
+    color: Colors.babyBlue,
+  },
+  backArrow: {
+    justifyContent: 'center',
+    width: 40,
+    height: 35,
+    backgroundColor: Colors.babyBlue,
+    borderBottomRightRadius: 100,
+    padding: 5,
+    marginBottom: 5,
   },
 });
 
