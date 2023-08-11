001db0ae0e39e79f19a0ffd5f5080b00ab1f6613
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-11 17:21:00 -0600

    Create a new component for the map location
    Created a new file for the map location component
    This component uses react-native-maps and the API from google maps to show the map and the location of the user
    It also shows the address of the location below the map

--- CtasApp/src/components/Map.tsx
+++ CtasApp/src/components/Map.tsx
@@ -1,10 +1,39 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import ProviderSetCitaStyles from "../styles/ProviderSetCitaStyles";
 
-const Map = () => {
+type Props = {
+  adress: string;
+};
+
+const Map: React.FC<Props> = props => {
+
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={ProviderSetCitaStyles.ubicationContainer}>
+      <View style={ProviderSetCitaStyles.mapContainer}>
+        <MapView
+            provider="google"
+            style={ProviderSetCitaStyles.map}
+            initialRegion={{
+              latitude: 19.2638,
+              longitude: -103.746,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}>
+              <Marker
+                coordinate={{
+                  latitude: 19.2638,
+                  longitude: -103.746,
+                }}
+                title="Ubicación"
+                description="Oficina"
+              />
+          </MapView>
+      </View>
+      <Text style={ProviderSetCitaStyles.ubicationText}>Adress: {props.adress}</Text>
     </View>
   );
 };
+
+export default Map;
