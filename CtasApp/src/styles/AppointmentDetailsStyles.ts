4f34e0531cadd3fd04a2222282ed6ed4690e7e6c
Tole15 <rodrigermoon@gmail.com>
2023-08-12 02:17:23 -0600

    In this commit, the AppointmentDetailsScreen has been completed, providing service providers with the ability to edit their appointment settings such as working hours, unavailable dates, and service locations. The screen leverages the custom components created earlier and integrates them for a seamless user experience. Here's the detailed breakdown:
    Integration of Components:
    
    AvailableTimePicker: Enables providers to select and manage their available time slots, generating intervals within a specified range.
    CustomCalendar: Offers an interactive calendar for selecting and marking unavailable dates, enhancing the scheduling process.
    AddressPicker: Provides an autocompleted input field, allowing providers to specify and select the location where the service is offered.
    Button: Utilizes reusable button components for various actions like saving changes.
    Functionality and User Interaction:
    
    The screen allows providers to easily navigate through different configurations, offering intuitive controls for adjusting appointment details.
    Providers can edit and review their settings all in one place, ensuring a streamlined process for managing appointments.
    Design and Layout:
    
    The layout of the screen is in line with the planned design and complements the overall aesthetic of the application.
    The use of custom components ensures a consistent look and feel, reflecting the application's branding and user interface guidelines.
    Final Touches:
    
    Final adjustments and refinements have been made to ensure the optimal functioning of the screen, taking into account user feedback and design principles.
    Thorough testing has been conducted to ensure that all integrated components work cohesively, delivering a reliable and efficient experience.
    Future Considerations:
    
    Further enhancements and customizations could be introduced based on user needs and feedback, allowing for continuous improvement and alignment with business goals.

---
+++ CtasApp/src/styles/AppointmentDetailsStyles.ts
@@ -0,0 +1,60 @@
+import {StyleSheet} from 'react-native';
+import Colors from './colors/Colors';
+
+const AppointmentDetailsStyles = StyleSheet.create({
+  background: {
+    backgroundColor: Colors.aqua,
+    flex: 1,
+  },
+  header: {
+    backgroundColor: Colors.aqua,
+    height: 100,
+  },
+  backArrow: {
+    justifyContent: 'center',
+    width: 70,
+    height: 55,
+    backgroundColor: Colors.babyBlue,
+    borderBottomRightRadius: 100,
+    paddingLeft: 8,
+    paddingBottom: 5,
+  },
+  container: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: Colors.white,
+  },
+  title: {
+    fontFamily: 'Inter-ExtraBold',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: Colors.white,
+    marginLeft: 20,
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: 'Inter-ExtraBold',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.babyBlue,
+    marginTop: 10,
+  },
+  labels: {
+    fontFamily: 'Inter-ExtraBold',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: Colors.babyBlue,
+    marginLeft: 40,
+    marginTop: 10,
+  },
+  addressContainer: {
+    paddingTop: 5,
+    paddingBottom: 10,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 60,
+  },
+});
+export default AppointmentDetailsStyles;
