720f69d2554620cab7eec159ed89322a2a4da9e8
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-15 15:16:32 -0600

    Fixed screen typo to avoid error

--- CtasApp/src/navigation/StackNavigation.tsx
+++ CtasApp/src/navigation/StackNavigation.tsx
@@ -15,7 +15,7 @@ import {RootStackParamList} from '../schema/SignInScreenSchema';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const StackNavigation: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="splash">
+    <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen
         name="Splash"
         component={SplashScreen}
