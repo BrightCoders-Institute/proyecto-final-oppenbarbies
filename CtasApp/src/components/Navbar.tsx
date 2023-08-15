b24c29e09fd55de1bc4b156c463f4f514548c6e3
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-15 11:43:27 -0600

    Modify the Navbar.tsx component
    Updated the Navbar.tsx component to include the following:
    * Change the button styles when a button is selected
    * Implement the useNavigation hook to navigate to the screens when the respective button is selected

--- CtasApp/src/components/Navbar.tsx
+++ CtasApp/src/components/Navbar.tsx
@@ -1,22 +1,67 @@
-import React from 'react';
-import {View, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {View, SafeAreaView, TouchableOpacity} from 'react-native';
 import NavbarStyles from '../styles/NavbarStyles';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {useNavigation} from '@react-navigation/native';
 
-const Navbar: React.FC = () => {
+const Navbar: React.FC = React.memo(() => {
+  const [selectedIcon, setSelectedIcon] = useState('user-alt');
+  const navigation = useNavigation<any>();
+
+  const handleIconPress = (iconName: string) => {
+    setSelectedIcon(iconName);
+
+    switch (iconName) {
+      case 'user-alt':
+        navigation.navigate('HomeClient');
+        break;
+      case 'calendar-day':
+        navigation.navigate('MyAppointments');
+        break;
+      case 'search':
+        navigation.navigate('SearchScreen');
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <SafeAreaView style={NavbarStyles.container}>
       <View style={NavbarStyles.navbar}>
-        <FontAwesome5 style={NavbarStyles.selected} name="user-alt" size={25} />
-        <FontAwesome5
-          style={NavbarStyles.unselected}
-          name="calendar-day"
-          size={25}
-        />
-        <FontAwesome5 style={NavbarStyles.unselected} name="search" size={25} />
+        <TouchableOpacity onPress={() => handleIconPress('user-alt')}>
+          <FontAwesome5 
+            style={[
+              NavbarStyles.icon,
+              selectedIcon === 'user-alt' ? NavbarStyles.selected : NavbarStyles.unselected,
+            ]}
+            name="user-alt" 
+            size={25}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleIconPress('calendar-day')}>
+          <FontAwesome5
+            style={[
+              NavbarStyles.icon,
+              selectedIcon === 'calendar-day' ? NavbarStyles.selected : NavbarStyles.unselected,
+            ]}
+            name="calendar-day"
+            size={25}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleIconPress('search')}>
+          <FontAwesome5 
+            style={[
+              NavbarStyles.icon,
+              selectedIcon === 'search' ? NavbarStyles.selected : NavbarStyles.unselected,
+            ]}
+            name="search" 
+            size={25} 
+          />
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
-};
+});
 
 export default Navbar;
