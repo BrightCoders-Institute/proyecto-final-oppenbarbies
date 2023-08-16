df2e557e1ca011bd8fde68e87c25d621318cf54c
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-16 17:08:48 -0600

    Get user info by auth
    Co-authored-by: Antonio3veces3 <aantonioramirez33@gmail.com>

--- CtasApp/src/components/ProfileClientSimpleCard.tsx
+++ CtasApp/src/components/ProfileClientSimpleCard.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
-import {ProfileClientSimpleCardProps} from '../schema/ProfileClientSimpleCardSchema';
 import ProfileClientSimpleCardStyles from '../styles/ProfileClientSimpleCardStyles';
+import auth from '@react-native-firebase/auth';
 
+type ProfileClientSimpleCardProps= {
+  userType: String | null;
+  userName: String | null;
+  userEmail: String | null;
+}
 const ProfileClientSimpleCard: React.FC<ProfileClientSimpleCardProps> = ({
-  userType = 'Client',
-  userName = 'Jessica Jhonson',
-  userEmail = 'jessica@correo.com',
+  userType,
+  userName,
+  userEmail,
 }) => {
+  
+
   return (
     <View style={ProfileClientSimpleCardStyles.cardContainer}>
       <Text style={ProfileClientSimpleCardStyles.userType}>{userType}</Text>
       <View style={ProfileClientSimpleCardStyles.userImageContainer}>
-        <Image
-          source={{
-            uri: 'https://images.pexels.com/photos/15212752/pexels-photo-15212752/free-photo-of-chica-bangkok.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-          }}
-          style={ProfileClientSimpleCardStyles.userImage}
-        />
+          <Image
+            source={{
+              uri: (user?.photoURL)?`${user?.photoURL}`: 'https://images.pexels.com/photos/15212752/pexels-photo-15212752/free-photo-of-chica-bangkok.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            }}
+            style={ProfileClientSimpleCardStyles.userImage}
+          />
       </View>
-      <Text style={ProfileClientSimpleCardStyles.userName}>{userName}</Text>
+      <Text style={ProfileClientSimpleCardStyles.userName}>
+        {user?.displayName}
+      </Text>
       <Text style={ProfileClientSimpleCardStyles.userEmail}>
-        Email: {userEmail}
+        Email: {user?.email}
       </Text>
     </View>
   );
--- CtasApp/src/screens/ProfileClientScreen.tsx
+++ CtasApp/src/screens/ProfileClientScreen.tsx
@@ -4,15 +4,18 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import ProfileClientScreenStyles from '../styles/ProfileClientScreenStyles';
 import ProfileClientSimpleCard from '../components/ProfileClientSimpleCard';
 import ProfileClientForm from '../components/ProfileClientForm';
+import auth from '@react-native-firebase/auth';
 
-const ProfileClientScreen: React.FC = () => {
+
+const ProfileClientScreen: React.FC = ({navigation, route}) => {
+  const user = auth().currentUser;
   return (
     <SafeAreaView style={ProfileClientScreenStyles.container}>
       <KeyboardAwareScrollView
         resetScrollToCoords={{x: 0, y: 0}}
         contentContainerStyle={ProfileClientScreenStyles.contentContainer}
         scrollEnabled={true}>
-        <ProfileClientSimpleCard />
+        <ProfileClientSimpleCard userType={userType} userName={displayName} userEmail={email}/>
         <ProfileClientForm />
       </KeyboardAwareScrollView>
     </SafeAreaView>
