67b4fa66230782557f27b0bf03212c1980a0c920
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-16 14:46:20 -0600

    Added AppointmentDetailsModal to AppointmentList
    Modified AppointmentList component to include AppointmentDetailsModal component
    This modal will be used to display the details of an appointment when the user clicks on the appointment in the list
    It gets the data from the AppointmentCard component and displays it in the modal

--- CtasApp/src/components/AppointmentList.tsx
+++ CtasApp/src/components/AppointmentList.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import {FlatList} from 'react-native';
+import {useState} from 'react';
+import {FlatList, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import AppointmentCard from './AppointmentCard';
 import {AppointmentListStyles as styles} from '../styles/AppointmentListStyles';
+import AppointmentDetailsModal from './AppointmentDetailsModal';
 import {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   PersonProvider,
@@ -126,20 +128,39 @@ const dataProviders: AppointmentProvider[] = [
 ];
 
 const AppointmentList: React.FC = () => {
+  const [selectedAppointment, setSelectedAppointment] = useState<AppointmentProvider | null>(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const openModal = (appointment: AppointmentProvider) => {
+    setSelectedAppointment(appointment);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedAppointment(null);
+    setModalVisible(false);
+  };
   return (
     <SafeAreaView style={styles.listContainer}>
       <FlatList
         data={dataProviders}
         renderItem={({item}) => (
-          <AppointmentCard
-            date={item.date}
-            time={item.time}
-            address={item.address}
-            person={item.person}
-          />
+          <TouchableOpacity onPress={() => openModal(item)}>
+            <AppointmentCard
+              date={item.date}
+              time={item.time}
+              address={item.address}
+              person={item.person}
+            />
+          </TouchableOpacity>
         )}
         keyExtractor={item => item.id}
         style={styles.flatList}
+      />
+      <AppointmentDetailsModal
+        isVisible={isModalVisible}
+        appointment={selectedAppointment}
+        onClose={closeModal}
       />
     </SafeAreaView>
   );
