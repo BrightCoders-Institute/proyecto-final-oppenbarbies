64b40f5f49d1eb6ecd380b762cd9241489dcfce6
Tole15 <rodrigermoon@gmail.com>
2023-08-16 02:12:19 -0600

    Refactored: Externalized Styles for Better Modularity
    Commit Description:
    In our continuous efforts to improve the codebase and enhance modularity, a significant refactoring has been done concerning the styles of our components:
    
    Separation of Styles:
    Purpose: To keep component logic and presentation layer distinct, the styles of each component have been separated into individual files.
    Directory Structure: Each style file is placed in a dedicated styles directory, ensuring organized storage and easy reference.
    Naming Convention: Adopted a consistent naming pattern, where the style file for a component named ComponentName would be named ComponentNameStyles.ts.
    Improved Code Readability:
    
    Clarity: By moving the styles out of component files, the component code has become more concise, focusing only on its core logic.
    Ease of Maintenance: With this separation, any updates or tweaks related to styling can be made in the style files without delving into the component logic.

--- CtasApp/src/components/TimeSlotButton.tsx
+++ CtasApp/src/components/TimeSlotButton.tsx
@@ -1,25 +1,30 @@
 import * as React from 'react';
-import { View,Text, Alert } from 'react-native';
+import {View, Text, Alert} from 'react-native';
 import TimeSlotsModal from './TimeSlotsModal';
 import Button from './Button';
+import {TimeSlotButtonProps} from '../schema/TimeSlotsSchema';
 
-interface TimeSlotButtonProps {
-  slots: string[];
-}
-
-const TimeSlotButton: React.FC<TimeSlotButtonProps> = ({ slots }) => {
+const TimeSlotButton: React.FC<TimeSlotButtonProps> = ({slots}) => {
   const [modalVisible, setModalVisible] = React.useState(false);
   const [selectedSlot, setSelectedSlot] = React.useState<string | null>(null);
 
   const handleSlotSelection = (timeSlot: string) => {
     setSelectedSlot(timeSlot);
     setModalVisible(false);
-    Alert.alert("Time Slot Selected", `You have selected the ${timeSlot} slot.`);
+    Alert.alert(
+      'Time Slot Selected',
+      `You have selected the ${timeSlot} slot.`,
+    );
   };
 
   return (
     <View>
-      <Button text="Select Time Slot" onPress={() => setModalVisible(true)} styleName='welcome' textStyleName='welcome'/>
+      <Button
+        text="Select Time Slot"
+        onPress={() => setModalVisible(true)}
+        styleName="welcome"
+        textStyleName="welcome"
+      />
 
       <TimeSlotsModal
         isVisible={modalVisible}
--- CtasApp/src/components/TimeSlotsModal.tsx
+++ CtasApp/src/components/TimeSlotsModal.tsx
@@ -5,16 +5,10 @@ import {
   Modal,
   FlatList,
   TouchableOpacity,
-  StyleSheet,
 } from 'react-native';
 import Button from './Button';
-
-interface TimeSlotsModalProps {
-  isVisible: boolean;
-  onClose: () => void;
-  slots: string[];
-  onSelect: (timeSlot: string) => void;
-}
+import {TimeSlotsModalProps} from '../schema/TimeSlotsSchema';
+import TimeSlotsModalStyles from '../styles/TimeSlotsModalStyles';
 
 const TimeSlotsModal: React.FC<TimeSlotsModalProps> = ({
   isVisible,
@@ -28,63 +22,25 @@ const TimeSlotsModal: React.FC<TimeSlotsModalProps> = ({
       transparent={true}
       visible={isVisible}
       onRequestClose={onClose}>
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <Text style={styles.title}>Choose the time for your appointment.</Text>
+      <View style={TimeSlotsModalStyles.modalContainer}>
+        <View style={TimeSlotsModalStyles.modalContent}>
+          <Text style={TimeSlotsModalStyles.title}>Choose the time for your appointment.</Text>
           <FlatList
             data={slots}
             keyExtractor={item => item}
             renderItem={({item}) => (
               <TouchableOpacity onPress={() => onSelect(item)}>
-                <Text style={styles.listItem}>{item}</Text>
+                <Text style={TimeSlotsModalStyles.listItem}>{item}</Text>
               </TouchableOpacity>
             )}
           />
-          <View style={styles.closeButton}><Button text="Close" onPress={onClose} /></View>
-          
-
+          <View style={TimeSlotsModalStyles.closeButton}><Button text="Close" onPress={onClose} /></View>
         </View>
       </View>
     </Modal>
   );
 };
 
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalContent: {
-    width: '80%',
-    height: '50%',
-    backgroundColor: '#40B9A9',
-    borderRadius: 10,
-    padding: 10,
-  },
-  title: {
-    color: 'white',
-    textAlign: 'center',
-    fontFamily: 'Inter-ExtraBold',
-    fontSize: 18,
-    paddingBottom: 10,
-    marginBottom: 10,
-    borderColor: '#eee',
-    borderBottomWidth: 5,
-  },
-  listItem: {
-    textAlign: 'center',
-    fontFamily: 'Inter-Bold',
-    padding: 10,
-    borderBottomWidth: 2,
-    borderBottomColor: '#eee',
-    color: 'white',
-  },
-  closeButton: {
-    marginVertical: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
 export default TimeSlotsModal;
+
+
