72c48fab859892607e0264deabfd927522ad8f20
Tole15 <rodrigermoon@gmail.com>
2023-08-16 21:20:25 -0600

    Set SplashScreen as Initial Route in StackNavigator.
    In line with enhancing the user experience right from the application's initiation, the following modifications have been executed:
    
    1. Initial Route Configuration:
       - The `SplashScreen` has been designated as the preliminary entry point within the `StackNavigator`. This ensures that users are greeted with a visually pleasing loading screen as the app initializes its necessary resources and configurations.

--- CtasApp/src/navigation/StackNavigation.tsx
+++ CtasApp/src/navigation/StackNavigation.tsx
@@ -16,7 +16,7 @@ import {RootStackParamList} from '../schema/SignInScreenSchema';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const StackNavigation: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="FinishAppointment">
+    <Stack.Navigator initialRouteName="Splash">
       <Stack.Screen
         name="Splash"
         component={SplashScreen}
