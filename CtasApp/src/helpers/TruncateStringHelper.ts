bd4e5a3134b7e487d5d3f9b63210c7c2dcaf3e20
Derek Ayala <derek.ayala12345@hotmail.com>
2023-08-17 16:57:09 -0600

    add a null check

--- CtasApp/src/helpers/TruncateStringHelper.ts
+++ CtasApp/src/helpers/TruncateStringHelper.ts
@@ -1,3 +1,3 @@
 export const truncateString = (str: string | null | undefined, num: number): string | null | undefined  => {
-  return str.length > num ? str.slice(0, num) + '...' : str;
+  return str?.length > num ? str?.slice(0, num) + '...' : str;
 };
