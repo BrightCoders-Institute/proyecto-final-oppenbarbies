01c63b578e038b9db282b79db0a7bdcfbbfc432a
Eduardoespl <eduardoesp3317@gmail.com>
2023-08-17 12:29:27 -0600

    Refactored AppointmentDetailsModal and added styles
    Refactored the structure of the AppointmentDetailsModal component to use the data from the card component, and added the description view.
    Added new styles for the AppointmentCard component.
    This styles centered the card and added the opacity effect to the background.
    Also changed the styles of the closeButton.

--- CtasApp/src/components/AppointmentDetailsModal.tsx
+++ CtasApp/src/components/AppointmentDetailsModal.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View, Text, Modal, Button, Image} from 'react-native';
+import {View, Text, Modal, TouchableOpacity, Image} from 'react-native';
 import { AppointmentProvider } from '../schema/AppointmentListSchema';
-import { AppointmentCardStyles } from '../styles/AppointmentCardStyles';
+import {AppointmentCardStyles as styles} from '../styles/AppointmentCardStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Colors from '../styles/colors/Colors';
 
 interface AppointmentDetailsModalProps {
   isVisible: boolean;
@@ -18,15 +20,53 @@ const AppointmentDetailsModal: React.FC<AppointmentDetailsModalProps> = ({
   }
 
   return (
-    <Modal visible={isVisible} animationType="slide">
-      <View>
-        <Text>Date: {appointment.date}</Text>
-        <Text>Time: {appointment.time}</Text>
-        <Text>Address: {appointment.address}</Text>
-        <Text>Person: {appointment.person.name}</Text>
-        <Text>Age: {appointment.person.age}</Text>
-        <Image source={require('../assets/profilePick.png')} />
-        <Button title="Close" onPress={onClose} />
+    <Modal transparent visible={isVisible} animationType="fade" style={styles.modal}>
+      <View style={styles.modal}>
+        <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+          <Icon name="close" size={25} color="white" />
+        </TouchableOpacity>
+        <View style={styles.modalContainer}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.txtDate}>Date</Text>
+            <Icon name="trash" size={25} style={styles.deleteIcon} color={'red'} />
+          </View>
+          <View style={styles.dateContainer}>
+            <Icon name="clock-o" size={18} style={styles.clockIcon} />
+            <Text style={styles.txtDateTime}>{appointment.date} | {appointment.time} hrs</Text>
+          </View>
+          <Text style={styles.divider} />
+
+          <View style={styles.footContainer}>
+            <View style={styles.personInfoContainer}>
+              <Image
+                source={{
+                  uri: 'https://images.pexels.com/photos/15212752/pexels-photo-15212752/free-photo-of-chica-bangkok.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                }}
+                style={styles.imgPerson}
+              />
+              <View style={styles.personInfo}>
+                <Text style={styles.txtName}>{appointment.person.name}</Text>
+                <Text style={styles.txtDescription}>
+                  Age: {appointment.person.age}
+                </Text>
+              </View>
+            </View>
+            <View style={styles.locationContainer}>
+              <Icon
+                name="map-pin"
+                size={18}
+                style={styles.locationIcon}
+                color={Colors.normalBlue}
+              />
+              <Text style={styles.txtLocation}>{appointment.address}</Text>
+            </View>
+          </View>
+          <Text style={styles.divider} />
+          <View style={styles.descriptionContainer}>
+              <Text style={styles.txtName}> Description</Text>
+              <Text style={styles.txtDescription}>{appointment.description}</Text>
+          </View>
+        </View>
       </View>
     </Modal>
   );
