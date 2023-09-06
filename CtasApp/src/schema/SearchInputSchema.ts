94233385ef4ab7852846059d9d940dcef3faf8f7
Derek Ayala <derek.ayala12345@hotmail.com>
2023-09-06 17:42:50 -0600

    new prop on SearchInput to use providers collection

--- CtasApp/src/schema/SearchInputSchema.ts
+++ CtasApp/src/schema/SearchInputSchema.ts
@@ -1,5 +1,8 @@
 import { Dispatch, SetStateAction } from "react";
+import {Provider} from '../schema/ProviderSchema';
+type ProviderWithKey = Provider & {key: string};
 export interface SearchInputProps {
   search: string,
   setSearch: Dispatch<SetStateAction<string>>,
+  providers: ProviderWithKey[],
 }
