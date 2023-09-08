1f86688532c816de9040983a14286a71a064ad6e
Antonio3veces3 <cramirez28@ucol.mx>
2023-09-08 13:32:24 -0600

    Add style to ConfigUnavailableDays Screen

--- CtasApp/src/screens/ConfigAvailableDays.tsx
+++ CtasApp/src/screens/ConfigAvailableDays.tsx
@@ -4,13 +4,16 @@ import BodyStyles from '../styles/BodyStyles';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import CustomCalendar from '../components/CustomCalendar';
+import {ConfigDayStyles as styles} from '../styles/ConfigDaysStyles/ConfigDaysStyles'
 
 const ConfigAvailableDays: React.FC = () => {
   return (
     <View style={BodyStyles.container}>
       <Header title="Set schedule" />
       <View style={BodyStyles.bodyContainer}>
-        <Text>Choose your unavailable days:</Text>
+        <View style={styles.subTitleContainer}>
+          <Text style={styles.text}>Choose your unavailable days:</Text>
+        </View>
         <CustomCalendar/>
         <Navbar />
       </View>
---
+++ CtasApp/src/styles/ConfigDaysStyles/ConfigDaysStyles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const ConfigDayStyles = StyleSheet.create({
+  subTitleContainer: {
+    height: 60,
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontFamily: 'Inter-ExtraBold',
+    marginTop: 30
+    },
+});
