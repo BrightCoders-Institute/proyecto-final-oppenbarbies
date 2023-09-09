b53cbe3ac885cb03210c6c64c6ba176e93858dac
Derek Ayala <derek.ayala12345@hotmail.com>
2023-09-09 09:52:29 -0600

    conection between sugestions and set cita screen

--- CtasApp/src/schema/ProvidersListSchema.ts
+++ CtasApp/src/schema/ProvidersListSchema.ts
@@ -2,4 +2,5 @@ import {Provider} from '../schema/ProviderSchema';
 type ProviderWithKey = Provider & {key: string};
 export interface ProvidersListProps {
   providers: ProviderWithKey[],
+  navigation: boolean
 }
