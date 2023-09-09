b6ff7425b3438d8e18e3300808b65092c49d340d
Derek Ayala <derek.ayala12345@hotmail.com>
2023-09-09 17:36:31 -0600

    change layout of provider information

--- CtasApp/src/components/ProviderInformation.tsx
+++ CtasApp/src/components/ProviderInformation.tsx
@@ -5,30 +5,38 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import StarRating from './SelectRate';
 import {ProviderInformationProps} from '../schema/ProviderInformationSchema';
 import Colors from '../styles/colors/Colors';
-import {SearchCardStyles} from '../styles/SearchCardStyles';
 
 const ProviderInformation: React.FC<ProviderInformationProps> = props => {
   return (
     <View style={ProviderSetCitaStyles.providerCard}>
-      <View>
-        <Image source={{uri: props.image}} style={SearchCardStyles.imgProfile} />
-      </View>
-      <View style={ProviderSetCitaStyles.detailsContainer}>
-        <Text style={ProviderSetCitaStyles.providerName}>{props.name}</Text>
-        <View style={ProviderSetCitaStyles.providerDataContainer}>
-          <Text style={ProviderSetCitaStyles.providerDataText}>
-            {props.location}
-          </Text>
+      <View style={ProviderSetCitaStyles.colmuns}>
+        <View>
+          <Image
+            source={{uri: props.image}}
+            style={ProviderSetCitaStyles.providerImage}
+          />
         </View>
-        <StarRating totalStars={5} />
-        <Text style={ProviderSetCitaStyles.servicesText}>
-          {props.description}
-        </Text>
         <View style={ProviderSetCitaStyles.socialMedia}>
           <FontAwesome name="facebook-square" size={24} color={Colors.white} />
           <FontAwesome name="whatsapp" size={24} color={Colors.white} />
+          <FontAwesome name="linkedin-square" size={24} color={Colors.white} />
+        </View>
+        <View style={ProviderSetCitaStyles.detailsContainer}>
+          <View style={ProviderSetCitaStyles.providerDataContainer}>
+            <Text style={ProviderSetCitaStyles.providerName}>{props.name}</Text>
+            <Text style={ProviderSetCitaStyles.providerOccupation}>
+              {props.occupation}
+            </Text>
+            <Text style={ProviderSetCitaStyles.providerDataText}>
+              {props.location}
+            </Text>
+            <StarRating totalStars={5} />
+          </View>
         </View>
       </View>
+      <Text style={ProviderSetCitaStyles.servicesText}>
+        {props.description}
+      </Text>
     </View>
   );
 };
