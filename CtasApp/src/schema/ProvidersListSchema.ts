74d5359c89661e5679f82f94c4442a655ff749f0
Tole15 <rodrigermoon@gmail.com>
2023-09-09 15:18:48 -0600

    Adjusted Types in Map Component
    In this update, the type definitions, particularly the 'address' type, have been modified to accept arrays. This change accommodates the possibility of a provider having multiple addresses. This adjustment aligns with the enhancements made in the Map component to better represent provider locations and enhance the user experience.

--- CtasApp/src/schema/ProvidersListSchema.ts
+++ CtasApp/src/schema/ProvidersListSchema.ts
@@ -2,5 +2,6 @@ import {Provider} from '../schema/ProviderSchema';
 type ProviderWithKey = Provider & {key: string};
 export interface ProvidersListProps {
   providers: ProviderWithKey[],
-  navigation: boolean
+  navigation: any,
 }
+
